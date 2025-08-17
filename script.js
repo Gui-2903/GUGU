19898ea0476587b0cbfd272ee7da7adca62ffe86
Guilherme <guilherme.1295232@discente.uemg.br>
2025-08-17 18:45:29 -0300

    inicio completo

---
+++ script.js
@@ -0,0 +1,74 @@
+// Inicializando o Zuck.js
+var stories = new Zuck('stories', {
+    skin: 'snapgram',   // snapgram, facegram, vemde, swipenight
+    avatars: true,
+    list: false,
+    cubeEffect: true,
+    autoFullScreen: false,
+    backNative: true,
+    previousTap: true,
+    localStorage: true,
+
+    stories: [
+        {
+            id: "user1",
+            photo: "https://randomuser.me/api/portraits/men/32.jpg",
+            name: "Guilherme",
+            link: "",
+            lastUpdated: Date.now(),
+            items: [
+                {
+                    id: "story1",
+                    type: "photo",
+                    length: 3,
+                    src: "https://picsum.photos/400/700?random=1",
+                    preview: "https://picsum.photos/100/150?random=1",
+                    seen: false,
+                    time: Date.now()
+                },
+                {
+                    id: "story2",
+                    type: "video",
+                    length: 0,
+                    src: "https://www.w3schools.com/html/mov_bbb.mp4",
+                    preview: "https://picsum.photos/100/150?random=2",
+                    seen: false,
+                    time: Date.now()
+                }
+            ]
+        },
+        {
+            id: "user2",
+            photo: "https://randomuser.me/api/portraits/women/44.jpg",
+            name: "Maria",
+            link: "",
+            lastUpdated: Date.now(),
+            items: [
+                {
+                    id: "story3",
+                    type: "photo",
+                    length: 5,
+                    src: "https://picsum.photos/400/700?random=3",
+                    preview: "https://picsum.photos/100/150?random=3",
+                    seen: false,
+                    time: Date.now()
+                }
+            ]
+        }
+    ]
+});
+
+window.onload = function () {
+    document.getElementById('mensagem-bemvindo').style.display = 'flex';
+};
+
+function fecharMensagem() {
+    document.getElementById('mensagem-bemvindo').style.display = 'none';
+}
+
+
+
+document.querySelector('.icone-animado').onclick = function () {
+    document.getElementById('audio-animado').play();
+};
+
